31f76d3f5e1521119bd3645183e7e3df65fe0f42
jameskinley <jameskinley24@gmail.com>
2024-11-13 17:34:40 +0000

    added ico

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from app import app
+from os import path
+from flask import send_from_directory, render_template
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(path.join(app.root_path, 'static'), 'favicon.ico')
 
 @app.route('/')
 def index():
-    return "Hello World!!!"
-
+    return render_template("home.html")
