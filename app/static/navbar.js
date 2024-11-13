53afb5d743820701005e6284c4202020fecfc0c9
jameskinley <jameskinley24@gmail.com>
2024-11-13 20:18:19 +0000

    nav working well

---
+++ app/static/navbar.js
@@ -0,0 +1,32 @@
+let icons =
+    [
+        {
+            id: "#nav-btn-home",
+            active: "/static/home-fill.svg",
+            inactive: "/static/home.svg"
+        },
+        {
+            id: "#nav-btn-profile",
+            active: "/static/person-fill.svg",
+            inactive: "/static/person.svg"
+        },
+        {
+            id: "#nav-btn-settings",
+            active: "/static/gear-fill.svg",
+            inactive: "/static/gear.svg"
+        }
+    ];
+
+function setInactiveIcons() {
+    icons.forEach(icon => {
+        $(icon.id).find("img").attr("src", icon.inactive);
+    });
+}
+
+$(document).ready(function () {
+    $(".navbar-item").click(function () {
+        setInactiveIcons();
+        let curr = `#${$(this).attr('id')}`;
+        $(this).find("img").attr("src", icons.find(icon => icon.id == curr).active);
+    });
+});
--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@ from app import app
 from os import path
 from flask import send_from_directory, render_template
 
+"""
+Serves the favicon request. 
+Code adapted from https://flask.palletsprojects.com/en/stable/patterns/favicon/
+"""
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(path.join(app.root_path, 'static'), 'favicon.ico')
