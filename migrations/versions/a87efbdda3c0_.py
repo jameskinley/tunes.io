9cede39f54d9f0f7e3c323fa5c404eaf174e0810
jameskinley <jameskinley24@gmail.com>
2024-11-25 23:07:18 +0000

    updated db. Ready for test development

---
+++ migrations/versions/a87efbdda3c0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a87efbdda3c0
+Revises: 44b5ea906d0f
+Create Date: 2024-11-25 23:06:15.124233
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a87efbdda3c0'
+down_revision = '44b5ea906d0f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
