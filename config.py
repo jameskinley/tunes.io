7f55d755b39d50e172bd46aae0773a2363726d55
James Kinley <jameskinley24@gmail.com>
2025-07-23 22:07:01 +0100

    sorted out visible API keys

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-SPOTIFY_API_ID = "40cf6eca2ed24e5ba0040de7dc954267"
-SPOTIFY_API_SECRET = "f5d738d86f0744a5b044b020ba5cbfe3" #If we have scope this should be hidden away
+SPOTIFY_API_ID = os.getenv("SPOTIFY_API_ID")
+SPOTIFY_API_SECRET = os.getenv("SPOTIFY_API_SECRET")
 SPOTIFY_LOCALE = "GB"
